f786686584d9a54b3a4715107f6a77687cc48b21
skfeat <128950274+skfeat@users.noreply.github.com>
2023-04-22 11:45:32 +0000

    done

--- src/components/RecentPlayer.js
+++ src/components/RecentPlayer.js
@@ -1,22 +1,16 @@
-// import React from 'react';
-// import { Grid } from '@mui/material';
+{loading ? (
+    <>
 
-// export default function RecentPlayer() {
-//   return (
-//     <Grid container justifyContent="center" sx={{ overflowX: 'hidden', marginTop: 2, marginLeft: 1, marginRight: 1 ,marginBottom:2}}>
-//       <Grid item xs={12} sm={8} md={6}>
-//         <video controls preload="auto" playsInline style={{
-//           width: '95.5%',
-//           borderRadius: '10px',
-//           boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.25)'
-//         }}>
-//           <source
-//             src="https://tc-002.adesicdn.com/1ab5d45273a9183bebb58eb74d5722d8ea6384f350caf008f08cf018f1f0566d0cb82a2a799830d1af97cd3f4b6a9a81ef3aed2fb783292b1abcf1b8560a1d1aa308008b88420298522a9f761e5aa1024fbe74e5aa853cfc933cd1219327d1232e91847a185021b184c027f97ae732b3708ee6beb80ba5db6628ced43f1196fe/0789fd4f049c3ca2a49b860ea5d1f456/ep.1.1677591537.360.m3u8"
-//             type="application/x-mpegURL"
-//           />
-//           Your browser does not support the video tag.
-//         </video>
-//       </Grid>
-//     </Grid>
-//   );
-// }
+      <Skeleton variant="rectangular" animation="wave" width="60%" height={10} sx={{ borderRadius: '7px', marginLeft: 1 }} />
+      <Skeleton variant="rectangular" animation="wave" width="60%" height={10} sx={{ borderRadius: '7px', marginLeft: 1 }} />
+      <Skeleton variant="rectangular" animation="wave" width="60%" height={10} sx={{ borderRadius: '7px', marginLeft: 1 }} />
+      <Skeleton variant="rectangular" animation="wave" width="60%" height={10} sx={{ borderRadius: '7px', marginLeft: 1 }} />
+      <Skeleton variant="rectangular" animation="wave" width="60%" height={10} sx={{ borderRadius: '7px', marginLeft: 1 }} />
+      <Skeleton variant="rectangular" animation="wave" width="10%" height={37} sx={{ borderRadius: '7px', marginLeft: 1 }} />
+      <Skeleton variant="rectangular" animation="wave" width="10%" height={37} sx={{ borderRadius: '7px', marginLeft: 1 }} />
+      <Skeleton variant="rectangular" animation="wave" width="10%" height={37} sx={{ borderRadius: '7px', marginLeft: 1 }} />
+      <Skeleton variant="rectangular" animation="wave" width="10%" height={37} sx={{ borderRadius: '7px', marginLeft: 1 }} />
+      <Skeleton variant="rectangular" animation="wave" width="10%" height={37} sx={{ borderRadius: '7px', marginLeft: 1 }} />
+    </>) : (
+                         {buttons}
+  )}
--- src/components/VideoPage.js
+++ src/components/VideoPage.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Grid } from '@mui/material';
-import { Skeleton } from '@mui/material';
-import { useParams } from 'react-router-dom';
+import { Skeleton, Button } from '@mui/material';
+import { useParams, Link } from 'react-router-dom';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function VideoPage() {
   const [videoSource, setVideoSource] = useState('');
   const [loading, setLoading] = useState(true);
+  const [title, setTitle] = useState('');
   const params = useParams();
-
+  const { episodeId } = params;
+  console.log(episodeId)
+  const words = episodeId.split("-");
+  const episodeNumber = parseInt(words[words.length - 1]);
+  const words1 = episodeId.split("-");
+  words1.pop(); // remove the last element from the array
+  const cleanEpisodeId = words1.join("-");
   useEffect(() => {
-    const { episodeId } = params;
-    console.log(episodeId)
-
     const fetchVideoSource = async () => {
       try {
         const response = await fetch(`https://gogoanime.consumet.stream/vidcdn/watch/${episodeId}`, {
@@ -23,7 +32,7 @@ export default function VideoPage() {
         const data = await response.json();
         const sourceUrl = data.Referer;
         const sourceUrl1 = data.sources_bk[0].file
-        console.log(sourceUrl1+"cors ki ma ki");
+        console.log(sourceUrl1 + "cors ki ma ki");
         console.log(data.Referer)
         setVideoSource(sourceUrl);
         setLoading(false);
@@ -32,21 +41,44 @@ export default function VideoPage() {
       }
     };
     fetchVideoSource();
-  }, [params]);
+    setTitle(
+      episodeId
+        .split("-")
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ")
+    );
+  }, [episodeId]);
 
-  const title = params.episodeId?.split("-")
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
-
+  const numButtons = episodeNumber
+  var buttonEpisodeId1 = `${cleanEpisodeId}-${episodeNumber - 1}`
+  var buttonEpisodeId2 = `${cleanEpisodeId}-${episodeNumber + 1}`
+  // Create an array of button components
+  const buttons = [];
+  for (let i = 1; i <= numButtons; i++) {
+    var buttonEpisodeId = `${cleanEpisodeId}-${i}`;
+    buttons.push(
+      <Link to={`/watch/${buttonEpisodeId}`} key={i}>
+        <Button variant="outlined" color="primary" sx={{ marginLeft: "5px", marginBottom: 1 }}>
+          Ep {i}
+        </Button>
+      </Link>
+    );
+  }
   return (
     <>
       <Grid container justifyContent="center" sx={{ overflowX: 'hidden', marginTop: 2 }}>
         <Grid item xs={12} sm={8} md={6}>
-          <h2 style={{ marginTop: 0, color: 'grey', paddingLeft: '0.5rem', marginBottom: -1 }}>{title}</h2>
+          {loading ? (
+            <>
+              <Skeleton variant="rectangular" animation="wave" width="90%" height={25} sx={{ borderRadius: '10px', marginBottom: 1, marginLeft: 1 }} />
+              <Skeleton variant="rectangular" animation="wave" width="70%" height={25} sx={{ borderRadius: '10px', marginLeft: 1 }} />
+            </>) : (
+            <h2 style={{ marginTop: 0, color: 'grey', paddingLeft: '0.5rem', marginBottom: -1 }}>{title}</h2>
+          )}
           <Grid container justifyContent="center" sx={{ overflowX: 'hidden', marginTop: 2, marginLeft: 1, marginRight: 1, marginBottom: 2 }}>
             <Grid item xs={12} sm={8} md={6}>
               {loading ? (
-                <Skeleton variant="rectangular" animation="wave" width="95%" height={260} sx={{borderRadius: '10px',}} />
+                <Skeleton variant="rectangular" animation="wave" width="95%" height={260} sx={{ borderRadius: '10px', }} />
               ) : (
                 <iframe
                   src={videoSource}
@@ -60,8 +92,55 @@ export default function VideoPage() {
                   }}
                 />
               )}
+
             </Grid>
           </Grid>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          {loading ? (
+    <>
+
+      <Skeleton variant="rectangular" animation="wave" width="25%" height={37} sx={{ borderRadius: '7px', marginLeft: 1 }} />
+    </>) : (
+            <Link to={`/watch/${buttonEpisodeId1}`}>
+            <Button variant="outlined" color="primary" sx={{ marginLeft: 1, marginBottom: 2 }}>
+              Previous
+            </Button>
+          </Link>
+  )}
+      {loading ? (
+    <>
+
+      <Skeleton variant="rectangular" animation="wave" width="25%" height={37} sx={{ borderRadius: '7px', marginRight: 1 }} />
+    </>) : (
+                 <Link to={`/watch/${buttonEpisodeId2}`}>
+                 <Button variant="outlined" color="primary" sx={{ marginRight: 1, marginBottom: 2 }}>
+                   Next
+                 </Button>
+               </Link>
+  )}
+          </div>
+          <Accordion sx={{marginTop:2}}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+           {loading ? (
+      <Skeleton
+        variant="rectangular"
+        animation="wave"
+        width="80%"
+        height={20}
+        sx={{ borderRadius: '7px', marginLeft: "1px" }}
+      />
+    ) : (
+      <Typography><b>Episodes List</b></Typography>
+    )}
+        </AccordionSummary>
+        <AccordionDetails>
+        {buttons}
+        </AccordionDetails>
+      </Accordion>
         </Grid>
       </Grid>
     </>
